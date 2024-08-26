9be8481118016b6c1833f2ff644005bbb0a9551b
luna1860 <saritaprinmc@gmail.com>
2024-08-26 09:38:53 -0500

    Añadí un input para que puedan ingresas una palabra

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # El siguiente programa recibe una entrada (input) del usuario y retorna el número de dígitos y de letras que contenga el mismo
 
 # Itendifica los errores y modifica el programa para que funcione según el enunciado
-user_input = "Dan1el"
+user_input = input("Ingrese una palabra")
 
 digits = 0
 letters = 0
