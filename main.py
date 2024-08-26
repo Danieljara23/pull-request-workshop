ded4bb2297c1fff1efb14e1f11f6b81d323a7843
b12s307 <nicolas.ruiiz07@gmail.com>
2024-08-26 09:28:07 -0500

    texto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # El siguiente programa recibe una entrada (input) del usuario y retorna el número de dígitos y de letras que contenga el mismo
 
 # Itendifica los errores y modifica el programa para que funcione según el enunciado
-user_input = input("ingrese su nombre: ")
+user_input = input("ingrese un texto: ")
 
 digits = 0
 letters = 0
